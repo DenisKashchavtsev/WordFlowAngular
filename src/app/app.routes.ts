1a81ce3504f8310ecebdbb14d8812c7ec1eded9e
Kashchavtsev Denis <dkashavtsev@gmail.com>
2024-01-24 00:39:47 +0200

    adding pages: category, dashboard, words, learning

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from "./pages/auth/login/login.component";
+import {DashboardComponent} from "./pages/dashboard/dashboard.component";
+import {LearningComponent} from "./pages/learning/learning.component";
+import {CategoryWordsComponent} from "./pages/category/category-words/category-words.component";
+import {StepsComponent} from "./pages/learning/steps/steps.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'dashboard', component: DashboardComponent},
+
+  {path: 'learning', component: LearningComponent},
+  {path: 'learn-category/:id', component: StepsComponent},
+
+  {path: 'category/:id', component: CategoryWordsComponent},
+];
